515448e8e57bd37657098ff9c6e5e1671cfa148d
z.suhadolnik <zane.suhadolnik@ineor.si>
2020-05-08 12:42:32 +0200

    Updated second page

---
+++ src/pages/page-2.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <div>Hello</div>
+  </Layout>
+)
+
+export default SecondPage
